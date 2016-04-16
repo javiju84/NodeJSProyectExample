2f611f56f599f4e5570747d6ce6f95e7e35120b0
Javier <javiju84@gmail.com>
2016-04-16 11:39:15 +0200

    express middlewares carpetas y archivos estáticos

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static('public'));  
+
+/*para montar un middlewares hay que pasarlo como parámetro al método "use" 
+sobre el objeto "app" que se crea cuando ejecutamos la función "express()"
+archivos estáticos o  static: imagenes, css , javascript. (no presenta compilación
+por parte del servidor, y por ello se llaman estático y van en una carpeta)
+por defecto se utiliza la carpeta 'public' pero puede generan tantas carpetas 
+necesitas para crear/guadar archivos estáticos.
+		 					*/
 app.set("view engine", "jade");
 
 //Verbos Http => GET /POST /PUT / PATCH / DELETE
