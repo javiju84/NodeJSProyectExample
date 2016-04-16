d5a49951e21bcb13ead3f6721a44ce8c89feab0c
Javier <javiju84@gmail.com>
2016-04-16 12:15:21 +0200

    middleware body-parser: buscar arcivos y extraerlos JSON

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
-
-app.use(express.static('public'));  
-app.use(express.static('assets'));  
+/*montamos los middlewares*/
 /*para montar un middlewares hay que pasarlo como parámetro al método "use" 
 sobre el objeto "app" que se crea cuando ejecutamos la función "express()"
 archivos estáticos o  static: imagenes, css , javascript. (no presenta compilación
 por parte del servidor, y por ello se llaman estático y van en una carpeta)
 por defecto se utiliza la carpeta 'public' pero puede generan tantas carpetas 
-necesitas para crear/guadar archivos estáticos.
-		 					*/
+necesitas para crear/guadar archivos estáticos.*/
+app.use("/public",express.static('public'));  
+app.use(express.static('assets'));  
+
+app.use(bodyParser.json());// para peticiones application/json
+app.use(bodyParser.urlencoded({extended: true})); 
+/*true o el false define el olgaritmo con que se va hacer el parsing la libreria,
+si el 'false' no se puede hacer parsing de array o parámetro que se envian de una 
+peticón get o post que no sean JSON*/
+/*"body-parser" buscar los archivos dentro de los datos y extraerlos
+  que vienen en una petición JSON*/
+
 app.set("view engine", "jade");
 
 //Verbos Http => GET /POST /PUT / PATCH / DELETE
@@ -22,8 +30,14 @@ app.get("/",function(req,res){
 });
 
 app.get("/login",function(req,res){
-	res.render("login");
+		res.render("login");
 });
 
-
+/*creamos la ruta login.jade*/
+/*post porque está definido en el login.jade "form(action="/users",method="POST")"*/
+app.post("/users", function(req,res){
+	console.log("Email: "+ req.body.email);
+	console.log("Contraseña: "+ req.body.password);/*confirmar que los parámetros se están leyendo */
+	res.send("Recibimos tus datos")
+});
 app.listen(8080);
